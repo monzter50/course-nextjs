62b270d51dda00fe00f574b55f48e21b4f090377
Jose Antonio <antonio.gonzalez@bdtravelsolutions.com>
2019-12-10 16:45:01 -0500

    add image for my hero

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import style from "./style.css"
+import style from "./style.css";
 function Home() {
-  return(
+  return (
     <div>
-      <h1 className={style.header}>Welcome to Next.js!</h1>  
-
+      <header
+        className={style.masthead}
+        style={{backgroundImage: "url("+'./static/home-bg.jpg'+")"}}
+      >
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="site-heading">
+                <h1>Clean Blog</h1>
+                <span className="subheading">
+                  A Blog Theme by Start Bootstrap
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <h1 className={style.header}>Welcome to Next.js!</h1>
     </div>
-  ) 
+  );
 }
 
-export default Home
+export default Home;
