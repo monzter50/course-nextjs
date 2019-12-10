7e32a4644c93a729dae128ef5bd83fe0fb85ccfb
Jose Antonio <antonio.gonzalez@bdtravelsolutions.com>
2019-12-10 15:15:21 -0500

    add css template blog

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
+import style from "./style.css"
 function Home() {
   return(
     <div>
-      <h1 className="holo">Welcome to Next.js!</h1>  
-      <style jsx>{`
-h1 {
-  color: white;
-}
-  div {
-    background: blue;
-  }
-`}</style>
-<style global jsx>{`
-    body {
-      background: red;
-    }
-`}</style>
+      <h1 className={style.header}>Welcome to Next.js!</h1>  
+
     </div>
   ) 
 }
