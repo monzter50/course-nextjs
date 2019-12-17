090c0a368a18e1ecdb519e9ee91222624f2e9961
Jose Antonio <antonio.gonzalez@bdtravelsolutions.com>
2019-12-17 17:51:08 -0500

    Get users and post respective id

---
+++ pages/details.js
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import style from "./style.css";
+function Details({post,user}) {
+
+  return (
+    <div>
+      <header className={style.masthead}>
+        <img src="./static/home-bg.jpg" className={style.mastheadImg} />
+        <div className={`${style.container} ${style.positionRelative}`}>
+          <div className={style.siteHeading}>
+            <h1> {user.name}</h1>
+            <span className={style.subheading}>
+              <Link href="/">
+                <a className={style.textWhite}>Back</a>
+              </Link>
+            </span>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className={`${style.container}`}>
+        <div style={{marginTop:40}} key={post.id}>
+              <h1 className={style.postTitle}>{post.title}</h1>
+              <article>
+                <p>{post.body}</p>
+  <p className={style.postMeta}><a href={`http://${user.website}`} target="_blank">@{user.username}</a></p>
+              </article>
+            </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+Details.getInitialProps = async ({ query }) => {
+  let idPost = query.id
+  let idUser = query.userId
+  const resPost = await fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`);
+  const jsonPost = await resPost.json();
+  const resUser = await fetch(`https://jsonplaceholder.typicode.com/users/${idUser}`);
+  const jsonUser = await resUser.json();
+  console.log(jsonUser)
+  return { post: jsonPost,user:jsonUser };
+};
+export default Details;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,43 @@
 import style from "./style.css";
-import Link from 'next/link'
-function Home() {
+import fetch from "isomorphic-unfetch";
+import Link from "next/link";
+function Home({ posts }) {
   return (
     <div>
-      <header
-        className={style.masthead}
-        style={{backgroundImage: "url("+'./static/home-bg.jpg'+")"}}
-      >
-        <div className="overlay"></div>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-md-10 mx-auto">
-              <div className="site-heading">
-                <h1>Clean Blog</h1>
-                <span className="subheading">
-                  A Blog Theme by Start Bootstrap
-                </span>
-              </div>
-            </div>
+      <header className={style.masthead}>
+        <img src="./static/home-bg.jpg" className={style.mastheadImg} />
+        <div className={`${style.container} ${style.positionRelative}`}>
+          <div className={style.siteHeading}>
+            <h1> My first blog with NextJs</h1>
+            <span className={style.subheading}>
+              A Blog Theme by Start Bootstrap
+            </span>
           </div>
         </div>
       </header>
-      <h1 className={style.header}>Welcome to Next.js!</h1>
-      <div>
-        <Link href={{ pathname: '/about', query: { name: 'Zeit' } }}>
-          <a>About Us</a>
-        </Link>
-      </div>
+      <main>
+        <div className={`${style.container}`}>
+          <h1 className={style.sectionHeading}>Welcome to Next.js!!</h1>
+
+          {posts.map(post => (
+            <div className={style.postPreview} key={post.id}>
+              <h1 className={style.postTitle}>{post.title}</h1>
+              <article>
+                <p>{post.body}</p>
+                <Link href={{ pathname: "/details", query: { id: post.id ,userId:post.userId} }}>
+                  <a className={`${style.btn} ${style.btnPrimary}`}>Read</a>
+                </Link>
+              </article>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
-
+Home.getInitialProps = async ({ req }) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const json = await res.json();
+  return { posts: json };
+};
 export default Home;
